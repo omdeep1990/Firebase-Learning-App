6c5233fa93962bced00d7cdb621904f530ccb9a3
Omdeep <omdeep1990@gmail.com>
2022-03-31 09:12:58 +0530

    Setup and connection of Firebase and insert operation done

---
+++ app/src/main/java/com/omdeep/firebaselearningapp/FirebaseDb.java
@@ -0,0 +1,16 @@
+package com.omdeep.firebaselearningapp;
+
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class FirebaseDb {
+    public DatabaseReference databaseReference;
+    public FirebaseDb() {
+        databaseReference = FirebaseDatabase.getInstance().getReference(StudentInfo.class.getSimpleName());
+    }
+
+    public Task<Void> add(StudentInfo studentInfo) {
+        return databaseReference.push().setValue(studentInfo);
+    }
+}
---
+++ app/src/main/java/com/omdeep/firebaselearningapp/MainActivity.java
@@ -0,0 +1,47 @@
+package com.omdeep.firebaselearningapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.omdeep.firebaselearningapp.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+    private FirebaseDb firebaseDb;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        firebaseDb = new FirebaseDb();
+        binding.btnInsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                StudentInfo studentInfo = new StudentInfo();
+                studentInfo.setFirstName(binding.etFname.getText().toString());
+                studentInfo.setLastName(binding.etLname.getText().toString());
+                studentInfo.setPhoneNumber(Long.parseLong(binding.etMobileno.getText().toString()));
+
+                firebaseDb.add(studentInfo).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        Utility.showLongToast(MainActivity.this, "Successfully Inserted...");
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Utility.showLongToast(MainActivity.this, e.getMessage());
+                    }
+                });
+
+            }
+        });
+    }
+}
